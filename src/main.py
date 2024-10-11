5e8736adf7d09b369c565a2182f8db30da33aa58
Vitaliy Davydov <ychebn1y@yandex.ru>
2024-10-11 19:47:59 +0300

    feat: create save to file

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import time
 
+from Utils.Utils import DataSaver
 from APIClient.APIClient import HTTPClientSync
 
 load_dotenv()
@@ -17,10 +18,12 @@ headers: dict = {"X-Auth-Token" : TOKEN}
 def main():
     start_time = time.time()
     client: HTTPClientSync = HTTPClientSync(BASE_URL_TEST)
-    # data = client.post(headers=headers)
-    # print(data)
-    data = client.get(headers=headers)
-    print(data)
+    get_data = client.get(headers=headers)
+    saver = DataSaver()
+    saver.create_output_folder()
+    saver.save_to_file('test1', get_data)
+    post_data = client.post(headers=headers)
+    saver.save_to_file('post', post_data)
     
     
 
