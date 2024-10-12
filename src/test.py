6ca5a3034f0ca5b0d412543ad5f08db6f894920a
Vitaliy Davydov <ychebn1y@yandex.ru>
2024-10-12 11:59:32 +0300

    feat: bugfix

--- src/test.py
+++ src/test.py
@@ -3,11 +3,21 @@ import os
 import time
 from Parser.Parser import Parser
 from Graphics.Visualizator import Visualizator
-from main import generate_game_state
+from DataClasses.Map import Map
 
+def generate_game_state(data_obj: Parser) -> Map:
+    CONSTANTS = data_obj.parse_constants()
+    gameMap = Map(CONSTANTS.mapSizeX, CONSTANTS.mapSizeY)
+    [gameMap.add_object(obj) for obj in data_obj.parse_anomalies()]
+    [gameMap.add_object(obj) for obj in data_obj.parse_enemies()]
+    [gameMap.add_object(obj) for obj in data_obj.parse_transports()]
+    [gameMap.add_object(obj) for obj in data_obj.parse_bounties()]
+    print(gameMap)
+    return gameMap
 
 def main():
-    for root, dirs, files in os.walk('C:/Users/kiril/Desktop/hacaton/DatsMagic/buf/round3'):
+    visualizer = Visualizator()
+    for root, dirs, files in os.walk('./buf/round3'):
         for index, file in enumerate(files):
             if index % 2 == 0:  # Проверяем, является ли индекс четным
                 file_path = os.path.join(root, file)
@@ -15,9 +25,7 @@ def main():
                 with open(file_path, 'r') as f:
                     data_obj = Parser(json.load(f))
                     game_map = generate_game_state(data_obj=data_obj)
-                    visualizer = Visualizator()
                     visualizer.visualize_objects(game_map.objects)
-                    print('boom')
                     time.sleep(1)
                 
 if __name__ == "__main__":
